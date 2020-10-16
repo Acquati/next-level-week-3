1a9c59d19d7e7f6ee16b2025bcc97feeb18131c1
Leandro Acquati <leandroacquati@gmail.com>
2020-10-16 01:54:56 -0300

    add layout landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
-
-interface TitleProps {
-  text: string
-}
-
-const Title = (props: TitleProps) => <h1>{props.text}</h1>
+import { FiArrowRight } from 'react-icons/fi'
+import './styles/global.css'
+import './styles/pages/landing.css'
+import logoImg from './images/logo.svg'
 
 const App = () =>
-  <div className="App">
-    <Title text="Hello World 1" />
+  <div id="page-landing">
+    <div className="content-wrapper">
+      <img src={logoImg} alt="Happy" />
+
+      <main>
+        <h1>Leve felicidade para o mundo</h1>
+        <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+      </main>
+
+      <div className="location">
+        <strong>Rio do Sul</strong>
+        <span>Santa Catarina</span>
+      </div>
+
+      <a href="/" className="enter-app">
+        <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+      </a>
+    </div>
   </div>
 
 export default App
