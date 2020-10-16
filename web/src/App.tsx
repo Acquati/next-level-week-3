c927d1b936d5a3545cb986b775c8edf07ea1275e
Leandro Acquati <leandroacquati@gmail.com>
2020-10-16 04:10:32 -0300

    aula 1

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,11 @@
 import React from 'react'
-import { FiArrowRight } from 'react-icons/fi'
 import './styles/global.css'
-import './styles/pages/landing.css'
-import logoImg from './images/logo.svg'
+import Routes from './routes'
 
-const App = () =>
-  <div id="page-landing">
-    <div className="content-wrapper">
-      <img src={logoImg} alt="Happy" />
-
-      <main>
-        <h1>Leve felicidade para o mundo</h1>
-        <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-      </main>
-
-      <div className="location">
-        <strong>Rio do Sul</strong>
-        <span>Santa Catarina</span>
-      </div>
-
-      <a href="/" className="enter-app">
-        <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-      </a>
-    </div>
-  </div>
+function App() {
+  return (
+    <Routes />
+  )
+}
 
 export default App
