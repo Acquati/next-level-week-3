3142d753d2493abd8c6f7d3638a815640fc88a70
Leandro Acquati <leandroacquati@gmail.com>
2020-10-22 22:01:56 -0300

    aula 3

--- web/src/pages/CreateOrphanage.tsx
+++ web/src/pages/CreateOrphanage.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Map, Marker, TileLayer } from 'react-leaflet'
+import { LeafletMouseEvent } from 'leaflet'
 import { FiPlus } from 'react-icons/fi'
 import Sidebar from '../components/Sidebar'
 import mapIcon from '../utils/mapIcon'
@@ -7,6 +8,12 @@ import mapIcon from '../utils/mapIcon'
 import '../styles/pages/create-orphanage.css'
 
 export default function CreateOrphanage() {
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
+
+  function handleMapClick(event: any) {
+    console.log(event.latlng)
+  }
+
   return (
     <div id="page-create-orphanage">
       <Sidebar />
@@ -20,13 +27,14 @@ export default function CreateOrphanage() {
               center={[-27.2092052, -49.6401092]}
               style={{ width: '100%', height: 280 }}
               zoom={15}
+              onClick={handleMapClick}
             >
               <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
               {/* <TileLayer
                 url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
               /> */}
 
-              <Marker interactive={false} icon={mapIcon} position={[-27.2092052, -49.6401092]} />
+              {/* <Marker interactive={false} icon={mapIcon} position={[-27.2092052, -49.6401092]} /> */}
             </Map>
 
             <div className="input-block">
